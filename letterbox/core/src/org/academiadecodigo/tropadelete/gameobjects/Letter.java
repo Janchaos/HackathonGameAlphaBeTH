e0a998c56ce95c2016d203b5ab87b8c35b24fc3e
Freitas <pfreitas96@hotmail.com>
2019-04-25 15:50:59 +0000

    letterbox class without condition if caught and letter class boolean

--- letterbox/core/src/org/academiadecodigo/tropadelete/gameobjects/Letter.java
+++ letterbox/core/src/org/academiadecodigo/tropadelete/gameobjects/Letter.java
@@ -5,7 +5,19 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class Letter extends GameObjects {
 
+
+    private boolean isCaugth;
+
     public Letter(Texture img, Rectangle hitbox) {
         super(img, hitbox);
+        this.isCaugth = false;
+    }
+
+    public void setCaugth() {
+        isCaugth = true;
+    }
+
+    public boolean isCaugth(){
+        return isCaugth;
     }
 }
