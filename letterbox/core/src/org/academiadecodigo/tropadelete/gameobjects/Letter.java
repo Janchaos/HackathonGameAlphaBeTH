ab8dae138758a14ff94caefb8e80aba74ceede65
Freitas <pfreitas96@hotmail.com>
2019-04-25 14:15:16 +0000

    Letter class and Enum class

--- letterbox/core/src/org/academiadecodigo/tropadelete/gameobjects/Letter.java
+++ letterbox/core/src/org/academiadecodigo/tropadelete/gameobjects/Letter.java
@@ -1,11 +1,24 @@
 package org.academiadecodigo.tropadelete.gameobjects;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Letter extends GameObjects {
 
-    public Letter(Texture img, Rectangle hitbox) {
-        super(img, hitbox);
+    private char stringLetter;
+    private float x;
+    private float y;
+    public static final float HEIGHT = 50;
+    public static final float WIDTH = 30;
+
+    public Letter(Texture img, char stringLetter, float x, float y) {
+        super(img, new Rectangle(x, y, WIDTH, HEIGHT));
+        this.x = x;
+        this.y = y;
+
+        this.stringLetter = stringLetter;
     }
+
+
 }
